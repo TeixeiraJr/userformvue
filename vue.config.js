f13df3543c152efaa97200f3390b136d02f8a14a
teixeirajr <teixeirajunior1710@gmail.com>
2022-08-28 02:02:37 -0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
+        https: true,
         port: process.env.PORT,
         allowedHosts: "all"
     }
